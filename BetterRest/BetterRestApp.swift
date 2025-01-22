5daa3c357f3d65aba4c54ecd6c549f8e86dc4aa7
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-22 11:54:08 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Amr El-Fiqi on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
