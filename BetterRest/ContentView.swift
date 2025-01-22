5c1228414ba9f1214ea6e455526d74019318eee1
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-22 19:42:01 +0200

    Setting up Basic UI
    Setting UI for stepper and date picker for hours and minutes. Imported the ML model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Variables
+    @State var wakeUpTime = Date.now
+    @State var sleepTime = 8.0
+    @State var coffeAmount = 1
+
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep?")
+                    .font(.headline)
+                Stepper("\(sleepTime.formatted())", value: $sleepTime,in: 2...12, step: 0.25)
+                
+                Text("Daily Coffe Intake?")
+                    .font(.headline)
+                Stepper("\(coffeAmount.formatted())", value: $coffeAmount,in: 1...12)
+                
+            }
+            .navigationTitle("Better Rest")
+            .toolbar{
+                Button("Calculate", action: calculateBedTime)
+            }
         }
-        .padding()
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
